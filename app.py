407f27807336cfa46754b78b83fb106adb4b53a8
Richard <richard.oliveira@ourolux.com.br>
2025-02-20 10:19:20 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,81 @@
+from flask import Flask, request, jsonify
+import requests
+import json
+from gevent.pywsgi import WSGIServer
+
+app = Flask(__name__)
+
+# URL do segundo sistema que recebe os dados como Query Params
+DESTINO_URL_ADD_CONTATOS = "https://ourolux.bitrix24.com.br/rest/15/1cp3l525ysrcdyhe/crm.lead.add.json"
+
+@app.route('/convertendoParaConcatos', methods=['POST'])
+def convert_and_forward():
+    
+    try:
+        # Obtendo o JSON enviado pelo primeiro sistema
+        json_data = request.get_json()
+     
+        if not json_data:
+            return jsonify({"erro": "Nenhum JSON recebido"}), 400
+        
+        with open("MicroServicos/exemploRDNEW.json", "w", encoding="utf-8") as json_file:
+            json.dump(json_data, json_file, indent=4, ensure_ascii=False)
+
+        Campos = ['NAME','HAS_EMAIL','EMAIL][0][VALUE','ADDRESS_PROVINCE','PHONE][0][VALUE']
+        Values = []
+
+        if json_data['leads'][0]['name'] != None:
+            Values.append(json_data['leads'][0]['name'])
+        else:
+            Values.append('')
+        
+        if json_data['leads'][0]['email'] != None:
+            Values.append('Y')
+            Values.append(json_data['leads'][0]['email'])
+        else:
+            Values.append('N')
+            Values.append('')
+
+        if json_data['leads'][0]['state'] != None:
+            Values.append(json_data['leads'][0]['state'])
+        else:
+            Values.append('')
+
+        if json_data['leads'][0]['personal_phone'] != None:
+            Values.append(json_data['leads'][0]['personal_phone'])
+        elif json_data['leads'][0]['mobile_phone'] != None:
+            Values.append(json_data['leads'][0]['mobile_phone'])
+        else:
+            Values.append('')
+
+        body = {
+				"fields":
+				{ 
+                    "TITLE": "Lead - RD Station",
+					"NAME": f"{Values[0]}",
+                    "HAS_EMAIL": f"{Values[1]}",
+                    "EMAIL": [ { "VALUE": f"{Values[2]}", "VALUE_TYPE": "WORK" } ],
+                    "ADDRESS_PROVINCE": f"{Values[3]}",
+                    "PHONE": [ { "VALUE": f"{Values[4]}", "VALUE_TYPE": "WORK" } ],	
+		    }
+        }        
+
+        print(Values)
+
+        # Requisição
+        response = requests.post(DESTINO_URL_ADD_CONTATOS, json=body)
+        
+        # Retornando a resposta do segundo sistema
+        return jsonify({
+            "status_code": response.status_code,
+            "response": response.json() if response.headers.get('Content-Type') == 'application/json' else response.text
+        })
+
+    
+    except Exception as e:
+        return jsonify({"erro": str(e)}), 500
+
+if __name__ == '__main__':
+    #app.run(host='0.0.0.0', port=5000, debug=True)
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
